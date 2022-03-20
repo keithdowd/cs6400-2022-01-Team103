064051473d99bc9b42a3c737395c69141625b32c
Uros <umilojkovic3@gatech.edu>
2022-03-20 10:01:28 -0400

    fixed create statements

--- Phase_2/team103_p2_schema.sql
+++ Phase_2/team103_p2_schema.sql
@@ -19,19 +19,21 @@ FLUSH PRIVILEGES;
 
 CREATE TABLE User (
     userID int(16) unsigned NOT NULL AUTO_INCREMENT,
-    email varchar('255'),
-    user_firstname varchar('255'),
-    user_lastname varchar('255'),
-    user_nickname varchar('255'),
-    user_password varchar('255'),
-    unrated_swaps varchar('255'),
-    unaccepted_swaps varchar('255'),
+    email varchar(255),
+    user_firstname varchar(255),
+    user_lastname varchar(255),
+    user_nickname varchar(255),
+    user_password varchar(255),
+    unrated_swaps varchar(255),
+    unaccepted_swaps varchar(255),
     user_rating float,
       
     -- foreign key(s):
+    postalcode varchar(255),
     constraint fk_User_postalcode_UserAddress_postalcode FOREIGN KEY (postalcode)
         REFERENCES UserAddress(postalcode),
-    constraint fk_User_phone_Phone_phone FOREIGN KEY (phone_number)
+    phone_number varchar(255),
+    constraint fk_User_phone_number_Phone_phone FOREIGN KEY (phone_number)
         REFERENCES UserAddress(phone_number),
 
     -- key(s):
@@ -41,31 +43,32 @@ CREATE TABLE User (
 );
 
 CREATE TABLE UserAddress (
-    PRIMARY KEY postalcode varchar('255'),
-    addr_city varchar('255'),
-    addr_state varchar('255'),
-    addr_latitude varchar('255'),
-    addr_longitude varchar('255')
+    PRIMARY KEY postalcode varchar(255),
+    addr_city varchar(255),
+    addr_state varchar(255),
+    addr_latitude varchar(255),
+    addr_longitude varchar(255)
 );
 
 CREATE TABLE Phone (
-    PRIMARY KEY phone_number varchar('255'),
-    phone_share varchar('255'),
-    phone_type varchar('255')
+    PRIMARY KEY phone_number varchar(255),
+    phone_share varchar(255),
+    phone_type varchar(255)
 );
 
 CREATE TABLE Item (
     itemNumber int(16) unsigned NOT NULL AUTO_INCREMENT ,
-    item_title varchar('255'),
-    item_condition varchar('255'),
-    item_description varchar('255'),
-    itemtype_name varchar('255'),
-    itemtype_description varchar('255'),
-    itemtype_platform varchar('255'),
-    itemtype_media varchar('255'),
+    item_title varchar(255),
+    item_condition varchar(255),
+    item_description varchar(255),
+    itemtype_name varchar(255),
+    itemtype_description varchar(255),
+    itemtype_platform varchar(255),
+    itemtype_media varchar(255),
     itemtype_piece_count int,
 
     -- foreign key(s):
+    email varchar(255),
     constraint fk_Item_email_User_email FOREIGN KEY (email)
         REFERENCES User(email),
 
@@ -80,48 +83,26 @@ CREATE TABLE Swap (
     swap_proposer_rating float,
     swap_date_responded date,
     swap_date_proposed date,
-    swap_status varchar('255'),
+    swap_status varchar(255),
     
     -- foreign key(s):
-    constraint fk_Swap_counterparty_User_email FOREIGN KEY (email)
+    counterparty_email varchar(255),
+    constraint fk_Swap_counterparty_User_email FOREIGN KEY (counterparty_email)
         REFERENCES User(email),
-    constraint fk_Swap_proposer_User_email FOREIGN KEY (email)
+
+    proposer_email varchar(255),
+    constraint fk_Swap_proposer_User_email FOREIGN KEY (proposer_email)
         REFERENCES User(email),
-    constraint fk_Swap_counterparty_item_Item_item_number FOREIGN KEY (itemNumber)
+
+    counterparty_itemNumber int(16),
+    constraint fk_Swap_counterparty_item_Item_item_number FOREIGN KEY (counterparty_itemNumber)
         REFERENCES Item(itemNumber),
-    constraint fk_Swap_proposed_item_Item_item_number FOREIGN KEY (itemNumber)
+
+    proposer_itemNumber int(16),
+    constraint fk_Swap_proposed_item_Item_item_number FOREIGN KEY (proposer_itemNumber)
         REFERENCES Item(itemNumber),
 
     -- key(s):
     PRIMARY KEY (swapID)
 
 );
-
--- CONSTRAINTS
--- Foreign Keys : fk_User_postalcode_UserAddress_postalcode 
---                fk_User_phone_Phone_phone                 
---                fk_Item_email_User_email                        
---                fk_Swap_counterparty_User_email
---                fk_Swap_proposer_User_email
---                fk_Swap_proposed_item_Item_item_number
---                fk_Swap_counterparty_item_Item_item_number
-
-ALTER TABLE User
-    ADD CONSTRAINT fk_User_postalcode_UserAddress_postalcode FOREIGN KEY(postalcode) 
-    REFERENCES UserAddress(postalcode) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT fk_User_phone_Phone_phone_number FOREIGN KEY(phone_number) 
-    REFERENCES Phone(phone_number) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE Item
-    ADD CONSTRAINT fk_Item_email_User_email FOREIGN KEY (email) 
-    REFERENCES User(email) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE Swap
-    ADD CONSTRAINT fk_Swap_counterparty_User_email FOREIGN KEY(email) 
-    REFERENCES User(email) ON UPDATE CASCADE,
-    ADD CONSTRAINT fk_Swap_proposer_User_email FOREIGN KEY(email) 
-    REFERENCES User(email) ON UPDATE CASCADE,
-    ADD CONSTRAINT fk_Swap_proposed_item_Item_item_number FOREIGN KEY(item_number) 
-    REFERENCES Item(item_number) ON UPDATE CASCADE,
-    ADD CONSTRAINT fk_Swap_counterparty_item_Item_item_number FOREIGN KEY(item_number) 
-    REFERENCES Item(item_number) ON UPDATE CASCADE;
