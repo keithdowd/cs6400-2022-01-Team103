1118dbcaca2f1bf38ed420134bf767b7be75035b
sabrinawang0118 <32717479+sabrinawang0118@users.noreply.github.com>
2022-04-08 13:43:57 -0400

    Update RateSwaps.py

--- Drafts/Sabrina/Phase 3/RateSwaps.py
+++ Drafts/Sabrina/Phase 3/RateSwaps.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import *
+from tkinter import ttk, StringVar, OptionMenu, Button
 from tkinter.font import BOLD
 
 ##############################
