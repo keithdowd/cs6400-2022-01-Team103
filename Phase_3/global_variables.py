a86fb3e038ecabb009e23801b97096ea3ec8c0a8
Keith Dowd <keith.dowd@gmail.com>
2022-04-12 16:47:12 -0400

    Added new configuration settings
    Added global database credentials and connection string. Also added global imports.

--- Phase_3/global_variables.py
+++ Phase_3/global_variables.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import mysql.connector
+import tkinter as tk
+from tkinter import ttk
+
+##############################
+# DATABASE
+##############################
+
+# Credentials
+USER = 'team103'
+PASSWORD = 'gatech123'
+HOST = '127.0.0.1'
+DATABASE = 'CS6400_spr22_team103'
+
+# Connection string
+cnx = mysql.connector.connect(
+  user=USER, 
+  password=PASSWORD,
+  host=HOST,
+  database=DATABASE)
+
+##############################
+# GLOBALS
+##############################
+
 global email_text
