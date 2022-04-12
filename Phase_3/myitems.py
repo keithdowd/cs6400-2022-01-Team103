f231987c5dfab3a74da7e9a87b20e8be10afcd83
Keith Dowd <keith.dowd@gmail.com>
2022-04-12 16:47:38 -0400

    Added reads from database
    Added backend queries to database and display to view

--- Phase_3/myitems.py
+++ Phase_3/myitems.py
@@ -1,5 +1,5 @@
-import tkinter as tk
-from tkinter import ttk
+from global_variables import *
+
 
 ##############################
 # CONFIGURATION
@@ -22,7 +22,7 @@ item_counts = {
   'Card Games': 0,
   'Computer Games': 0,
   'Jigsaw Puzzles': 0,
-  'Video games': 0,
+  'Video Games': 0,
   'Total': 0
 }
 
@@ -36,21 +36,24 @@ my_items_columns = [
   ''
 ]
 
-my_items_data = [
-  [
-    23, 'Video game', 'Tetris', 'Lightly used', '', 'Detail'
-  ],
-  [
-    34, 'Board game', 'Monopoly', 'Damaged/Missing parts', 'We used to play this game heavily, and started losing some of the figurines, the cards, and finally...', 'Detail'
-  ],
-  [
-    106, 'Card game', 'UNO', 'Mint', 'Never opened!', 'Detail'
-  ]
-]
+my_items_data = []
+
+# my_items_data = [
+#   [
+#     23, 'Video game', 'Tetris', 'Lightly used', '', 'Detail'
+#   ],
+#   [
+#     34, 'Board game', 'Monopoly', 'Damaged/Missing parts', 'We used to play this game heavily, and started losing some of the figurines, the cards, and finally...', 'Detail'
+#   ],
+#   [
+#     106, 'Card game', 'UNO', 'Mint', 'Never opened!', 'Detail'
+#   ]
+# ]
 
 ##############################
 # SETUP
 ##############################
+
 def setup(title='My Window', width=800, height=400):
   window = tk.Tk()
   window.title(title)
@@ -59,9 +62,39 @@ def setup(title='My Window', width=800, height=400):
 
 window = setup(title=WINDOW_TITLE, width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
 
+
 ##############################
 # ITEM COUNTS
 ##############################
+
+########## DATA
+
+q = f'''
+  SELECT 
+        itemtype_name, 
+        count(*) as count
+    FROM {DATABASE}.item
+GROUP BY itemtype_name
+'''
+
+df = pd.read_sql_query(q, cnx)
+
+board_games_cnt = 0 if df[df['itemtype_name'] == 'Board Game'].empty else df[df['itemtype_name'] == 'Board Game']['count'].reset_index(drop=True)[0]
+card_games_cnt = 0 if df[df['itemtype_name'] == 'Card Game'].empty else df[df['itemtype_name'] == 'Card Game']['count'].reset_index(drop=True)[0]
+computer_games_cnt = 0 if df[df['itemtype_name'] == 'Computer Game'].empty else df[df['itemtype_name'] == 'Computer Game']['count'].reset_index(drop=True)[0]
+jigsaw_puzzles_cnt = 0 if df[df['itemtype_name'] == 'Jigsaw Puzzle'].empty else df[df['itemtype_name'] == 'Jigsaw Puzzle']['count'].reset_index(drop=True)[0]
+video_games_cnt = 0 if df[df['itemtype_name'] == 'Video Game'].empty else df[df['itemtype_name'] == 'Video Game']['count'].reset_index(drop=True)[0]
+total = board_games_cnt + card_games_cnt + computer_games_cnt + jigsaw_puzzles_cnt + video_games_cnt
+
+item_counts['Board Games'] = board_games_cnt
+item_counts['Card Games'] = card_games_cnt
+item_counts['Computer Games'] = computer_games_cnt
+item_counts['Jigsaw Puzzles'] = jigsaw_puzzles_cnt
+item_counts['Video Games'] = video_games_cnt
+item_counts['Total'] = total
+
+########## VIEW
+
 # Header
 label_item_counts = tk.Label(master=window, text='Item Counts')
 label_item_counts.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, anchor='w')
@@ -83,9 +116,44 @@ for column in table_item_counts['columns']:
 table_item_counts.insert(parent='', index='end', iid=0, text='', values=tuple(item_counts.values()))
 table_item_counts.pack()
 
+
 ##############################
 # MY ITEMS
 ##############################
+
+########## DATA
+
+q = f'''
+  SELECT
+        itemNumber,
+        itemtype_name,
+        item_title,
+        item_condition,
+        item_description
+    FROM {DATABASE}.item
+ORDER BY itemNumber ASC 
+'''
+
+df = pd.read_sql_query(q, cnx)
+
+for index, row in df.iterrows():
+    item_number = row['itemNumber']
+    item_type_name = row['itemtype_name']
+    item_title = row['item_title']
+    item_condition = row['item_condition']
+    item_description = row['item_description']
+    
+    arr = [
+        item_number,
+        item_type_name,
+        item_title,
+        item_condition,
+        item_description
+    ]
+
+    my_items_data.append(arr)
+
+########## VIEW
 
 # Header
 label_item_counts = tk.Label(master=window, text='My Items')
@@ -108,6 +176,7 @@ for column in table_my_items['columns']:
 for i, row in enumerate(my_items_data):
   table_my_items.insert(parent='', index='end', iid=i, text='', values=row)
 table_my_items.pack()
+
 
 ##############################
 # EVENT LOOP
