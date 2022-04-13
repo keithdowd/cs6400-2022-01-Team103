7b60d5be1857b936aaef8eb066c814ea4a484706
Keith Dowd <keith.dowd@gmail.com>
2022-04-12 23:55:06 -0400

    Pulled out globals from individual files

--- Phase_3/global_variables.py
+++ Phase_3/global_variables.py
@@ -21,7 +21,25 @@ cnx = mysql.connector.connect(
   database=DATABASE)
 
 ##############################
-# GLOBALS
+# VIEW
 ##############################
+
+# Buttons
+BUTTON_FONT_FAMILY = 'Courier'
+BUTTON_FONT_SIZE = 14
+BUTTON_FONT_WEIGHT = 'bold'
+
+# Labels
+LABEL_FONT_FAMILY = 'Courier'
+LABEL_FONT_SIZE = 10
+LABEL_FONT_WEIGHT_LABEL = 'bold'
+LABEL_FONT_WEIGHT_VALUE = 'normal'
+
+# Windows
+WINDOW_PADDING_X = 5
+WINDOW_PADDING_X_OFFSET = 42
+WINDOW_PADDING_Y = 5
+WINDOW_SIZE_HEIGHT = 400
+WINDOW_SIZE_WIDTH = 800
 
 global email_text
