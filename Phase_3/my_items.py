2a4b76ab91bb714011abe8cc41ee70ab9861ff3c
Keith Dowd <keith.dowd@gmail.com>
2022-04-12 23:55:36 -0400

    Wrapped windows in call functions

--- Phase_3/my_items.py
+++ Phase_3/my_items.py
@@ -1,155 +1,152 @@
 from global_variables import *
-import sql
+from sql import sql__my_items__count_of_item_type
+from sql import sql__my_items__list_of_all_items
+from view_item import view_item
+
+def my_items():
+  ##############################
+  # CONFIGURATION
+  ##############################
+
+  # Window
+  WINDOW_TITLE = 'My Items'
+
+  # Tables
+  TABLE_COLUMN_ANCHOR = tk.W
+  TABLE_COLUMN_STRETCH = tk.NO
+
+  # Item counts
+  item_counts = {
+    'Board Games': 0,
+    'Card Games': 0,
+    'Computer Games': 0,
+    'Jigsaw Puzzles': 0,
+    'Video Games': 0,
+    'Total': 0
+  }
+
+  # My items
+  my_items_columns = [
+    'Item #',
+    'Game Type',
+    'Title',
+    'Condition',
+    'Description',
+  ]
+
+  my_items_data = []
 
 
-##############################
-# CONFIGURATION
-##############################
+  ##############################
+  # SETUP
+  ##############################
 
-# Window
-WINDOW_PADDING_X = 5
-WINDOW_PADDING_Y = 5
-WINDOW_SIZE_HEIGHT = 400
-WINDOW_SIZE_WIDTH = 800
-WINDOW_TITLE = 'My Items'
+  def setup(title='My Window', width=800, height=400):
+    window = tk.Tk()
+    window.title(title)
+    window.geometry(f'{width}x{height}')
+    return window
 
-# Tables
-TABLE_COLUMN_ANCHOR = tk.W
-TABLE_COLUMN_STRETCH = tk.NO
-
-# Item counts
-item_counts = {
-  'Board Games': 0,
-  'Card Games': 0,
-  'Computer Games': 0,
-  'Jigsaw Puzzles': 0,
-  'Video Games': 0,
-  'Total': 0
-}
-
-# My items
-my_items_columns = [
-  'Item #',
-  'Game Type',
-  'Title',
-  'Condition',
-  'Description',
-  ''
-]
-
-my_items_data = []
+  window = setup(title=WINDOW_TITLE, width=WINDOW_SIZE_WIDTH, height=WINDOW_SIZE_HEIGHT)
 
 
-##############################
-# SETUP
-##############################
+  ##############################
+  # ITEM COUNTS
+  ##############################
 
-def setup(title='My Window', width=800, height=400):
-  window = tk.Tk()
-  window.title(title)
-  window.geometry(f'{width}x{height}')
-  return window
+  ########## DATA
+  df = pd.read_sql_query(sql__my_items__count_of_item_type, cnx)
 
-window = setup(title=WINDOW_TITLE, width=WINDOW_SIZE_WIDTH, height=WINDOW_SIZE_HEIGHT)
+  board_games_cnt = 0 if df[df['itemtype_name'] == 'Board Game'].empty else df[df['itemtype_name'] == 'Board Game']['count'].reset_index(drop=True)[0]
+  card_games_cnt = 0 if df[df['itemtype_name'] == 'Card Game'].empty else df[df['itemtype_name'] == 'Card Game']['count'].reset_index(drop=True)[0]
+  computer_games_cnt = 0 if df[df['itemtype_name'] == 'Computer Game'].empty else df[df['itemtype_name'] == 'Computer Game']['count'].reset_index(drop=True)[0]
+  jigsaw_puzzles_cnt = 0 if df[df['itemtype_name'] == 'Jigsaw Puzzle'].empty else df[df['itemtype_name'] == 'Jigsaw Puzzle']['count'].reset_index(drop=True)[0]
+  video_games_cnt = 0 if df[df['itemtype_name'] == 'Video Game'].empty else df[df['itemtype_name'] == 'Video Game']['count'].reset_index(drop=True)[0]
+  total = board_games_cnt + card_games_cnt + computer_games_cnt + jigsaw_puzzles_cnt + video_games_cnt
 
+  item_counts['Board Games'] = board_games_cnt
+  item_counts['Card Games'] = card_games_cnt
+  item_counts['Computer Games'] = computer_games_cnt
+  item_counts['Jigsaw Puzzles'] = jigsaw_puzzles_cnt
+  item_counts['Video Games'] = video_games_cnt
+  item_counts['Total'] = total
 
-##############################
-# ITEM COUNTS
-##############################
+  ########## VIEW
 
-########## DATA
+  # Header
+  label_item_counts = tk.Label(master=window, text='Item Counts')
+  label_item_counts.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, anchor='w')
 
-df = pd.read_sql_query(sql.my_items__count_of_item_type, cnx)
+  # Separator
+  separator = ttk.Separator(window, orient='horizontal')
+  separator.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, fill='x')
 
-board_games_cnt = 0 if df[df['itemtype_name'] == 'Board Game'].empty else df[df['itemtype_name'] == 'Board Game']['count'].reset_index(drop=True)[0]
-card_games_cnt = 0 if df[df['itemtype_name'] == 'Card Game'].empty else df[df['itemtype_name'] == 'Card Game']['count'].reset_index(drop=True)[0]
-computer_games_cnt = 0 if df[df['itemtype_name'] == 'Computer Game'].empty else df[df['itemtype_name'] == 'Computer Game']['count'].reset_index(drop=True)[0]
-jigsaw_puzzles_cnt = 0 if df[df['itemtype_name'] == 'Jigsaw Puzzle'].empty else df[df['itemtype_name'] == 'Jigsaw Puzzle']['count'].reset_index(drop=True)[0]
-video_games_cnt = 0 if df[df['itemtype_name'] == 'Video Game'].empty else df[df['itemtype_name'] == 'Video Game']['count'].reset_index(drop=True)[0]
-total = board_games_cnt + card_games_cnt + computer_games_cnt + jigsaw_puzzles_cnt + video_games_cnt
-
-item_counts['Board Games'] = board_games_cnt
-item_counts['Card Games'] = card_games_cnt
-item_counts['Computer Games'] = computer_games_cnt
-item_counts['Jigsaw Puzzles'] = jigsaw_puzzles_cnt
-item_counts['Video Games'] = video_games_cnt
-item_counts['Total'] = total
-
-########## VIEW
-
-# Header
-label_item_counts = tk.Label(master=window, text='Item Counts')
-label_item_counts.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, anchor='w')
-
-# Separator
-separator = ttk.Separator(window, orient='horizontal')
-separator.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, fill='x')
-
-# Table
-frame_item_counts = tk.Frame(window)
-frame_item_counts.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, anchor='w')
-table_item_counts = ttk.Treeview(frame_item_counts, height=1)
-table_item_counts['columns'] = tuple(item_counts.keys())
-table_item_counts.column('#0', width=0, stretch=TABLE_COLUMN_STRETCH)
-table_item_counts.heading('#0', text='')
-for column in table_item_counts['columns']:
-  table_item_counts.column(column, anchor=TABLE_COLUMN_ANCHOR, width=int(WINDOW_SIZE_WIDTH/len(table_item_counts['column']))-2)
-  table_item_counts.heading(column, text=column, anchor=TABLE_COLUMN_ANCHOR)
-table_item_counts.insert(parent='', index='end', iid=0, text='', values=tuple(item_counts.values()))
-table_item_counts.pack()
+  # Table
+  frame_item_counts = tk.Frame(window)
+  frame_item_counts.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, anchor='w')
+  table_item_counts = ttk.Treeview(frame_item_counts, height=1)
+  table_item_counts['columns'] = tuple(item_counts.keys())
+  table_item_counts.column('#0', width=0, stretch=TABLE_COLUMN_STRETCH)
+  table_item_counts.heading('#0', text='')
+  for column in table_item_counts['columns']:
+    table_item_counts.column(column, anchor=TABLE_COLUMN_ANCHOR, width=int(WINDOW_SIZE_WIDTH/len(table_item_counts['column']))-2)
+    table_item_counts.heading(column, text=column, anchor=TABLE_COLUMN_ANCHOR)
+  table_item_counts.insert(parent='', index='end', iid=0, text='', values=tuple(item_counts.values()))
+  table_item_counts.pack()
 
 
-##############################
-# MY ITEMS
-##############################
+  ##############################
+  # MY ITEMS
+  ##############################
 
-########## DATA
+  ########## DATA
 
-df = pd.read_sql_query(sql.my_items__list_of_all_items, cnx)
+  df = pd.read_sql_query(sql__my_items__list_of_all_items, cnx)
 
-for index, row in df.iterrows():
-    item_number = row['itemNumber']
-    item_type_name = row['itemtype_name']
-    item_title = row['item_title']
-    item_condition = row['item_condition']
-    item_description = row['item_description']
-    
-    arr = [
-        item_number,
-        item_type_name,
-        item_title,
-        item_condition,
-        item_description
-    ]
+  for index, row in df.iterrows():
+      item_number = row['itemNumber']
+      item_type_name = row['itemtype_name']
+      item_title = row['item_title']
+      item_condition = row['item_condition']
+      item_description = row['item_description']
+      
+      arr = [
+          item_number,
+          item_type_name,
+          item_title,
+          item_condition,
+          item_description
+      ]
 
-    my_items_data.append(arr)
+      my_items_data.append(arr)
 
-########## VIEW
+  ########## VIEW
 
-# Header
-label_item_counts = tk.Label(master=window, text='My Items')
-label_item_counts.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, anchor='w')
+  # Header
+  label_item_counts = tk.Label(master=window, text='My Items')
+  label_item_counts.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, anchor='w')
 
-# Separator
-separator = ttk.Separator(window, orient='horizontal')
-separator.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, fill='x')
+  # Separator
+  separator = ttk.Separator(window, orient='horizontal')
+  separator.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, fill='x')
 
-# Table
-frame_my_items = tk.Frame(window)
-frame_my_items.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, anchor='w')
-table_my_items = ttk.Treeview(frame_my_items, height=len(my_items_data))
-table_my_items ['columns'] = my_items_columns
-table_my_items.column('#0', width=0, stretch=TABLE_COLUMN_STRETCH)
-table_my_items.heading('#0', text='')
-for column in table_my_items['columns']:
-  table_my_items.column(column, anchor=TABLE_COLUMN_ANCHOR, width=int(WINDOW_SIZE_WIDTH/len(table_my_items['column']))-2)
-  table_my_items.heading(column, text=column, anchor=TABLE_COLUMN_ANCHOR)
-for i, row in enumerate(my_items_data):
-  table_my_items.insert(parent='', index='end', iid=i, text='', values=row)
-table_my_items.pack()
+  # Table
+  frame_my_items = tk.Frame(window)
+  frame_my_items.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, anchor='w')
+  table_my_items = ttk.Treeview(frame_my_items, height=len(my_items_data))
+  table_my_items ['columns'] = my_items_columns
+  table_my_items.column('#0', width=0, stretch=TABLE_COLUMN_STRETCH)
+  table_my_items.heading('#0', text='')
+  for column in table_my_items['columns']:
+    table_my_items.column(column, anchor=TABLE_COLUMN_ANCHOR, width=int(WINDOW_SIZE_WIDTH * 0.15))
+    table_my_items.heading(column, text=column, anchor=TABLE_COLUMN_ANCHOR)
+  for i, row in enumerate(my_items_data):
+    table_my_items.insert(parent='', index='end', iid=i, text='', values=row)
+    tk.Button(window, text="Details", command=lambda item_number=my_items_data[i][0]: view_item(item_number)).pack(side=tk.RIGHT)
+  table_my_items.pack()
 
 
 ##############################
 # EVENT LOOP
 ##############################
-window.mainloop()
+# window.mainloop()
--- Phase_3/view_item.py
+++ Phase_3/view_item.py
@@ -1,128 +1,118 @@
-import tkinter as tk
-from tkinter import ttk
+from global_variables import *
+from sql import sql__view_items__item_details
 
-##############################
-# CONFIGURATION
-##############################
+def view_item(item_number):
 
-# Window
-WINDOW_TITLE = 'Propose Swap'
+  ##############################
+  # CONFIGURATION
+  ##############################
 
-WINDOW_HEIGHT = 400
-WINDOW_WIDTH = 800
+  # Window
+  WINDOW_TITLE = 'Propose Swap'
 
-WINDOW_PADDING_X = 5
-WINDOW_PADDING_X_OFFSET = 42
-WINDOW_PADDING_Y = 5
+  ##############################
+  # SETUP
+  ##############################
+  def setup(title='My Window', width=800, height=400):
+    window = tk.Tk()
+    window.title(title)
+    window.geometry(f'{width}x{height}')
+    return window
 
-# Font
-LABEL_FONT_FAMILY = 'Courier'
-LABEL_FONT_SIZE = 10
-LABEL_FONT_WEIGHT_LABEL = 'bold'
-LABEL_FONT_WEIGHT_VALUE = 'normal'
+  window = setup(title=WINDOW_TITLE, width=WINDOW_SIZE_WIDTH, height=WINDOW_SIZE_HEIGHT)
 
-BUTTON_FONT_FAMILY = 'Courier'
-BUTTON_FONT_SIZE = 14
-BUTTON_FONT_WEIGHT = 'bold'
+  ##############################
+  # VIEW ITEM
+  ##############################
 
-##############################
-# SETUP
-##############################
-def setup(title='My Window', width=800, height=400):
-  window = tk.Tk()
-  window.title(title)
-  window.geometry(f'{width}x{height}')
-  return window
+  ########## DATA
+  df = pd.read_sql_query(sql__view_items__item_details(item_number), cnx)
 
-window = setup(title=WINDOW_TITLE, width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+  itemNumber = df['itemNumber'].values[0]
+  item_title = df['item_title'].values[0]
+  itemtype_name = df['itemtype_name'].values[0]
+  itemtype_platform = df['itemtype_platform'].values[0]
+  itemtype_media = df['itemtype_media'].values[0]
+  item_condition = df['item_condition'].values[0]
 
-##############################
-# PROPOSE SWAP
-##############################
+  # OFFERED_BY = 'Calamity G.'
+  # LOCATION = 'Redmond, WA 98052'
+  # RATING = 0.45
+  # DISTANCE = 2168.1
 
-# Header
-label_item_counts = tk.Label(master=window, text='Item Details', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
-label_item_counts.grid(row=0, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  ########## VIEW
 
- # Separator
-separator = ttk.Separator(window, orient='horizontal')
-separator.grid(row=1, columnspan=4, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='ew')
+  # Header
+  label_item_counts = tk.Label(master=window, text='Item Details', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
+  label_item_counts.grid(row=0, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
 
-# Globals
-ITEM_NUMBER = 3
-TITLE = 'Legend of Zelda: Breath of the Wild'
-GAME_TYPE = 'Video game'
-PLATFORM = 'Nintendo'
-MEDIA = 'Game card'
-CONDITION = 'Lightly used'
-OFFERED_BY = 'Calamity G.'
-LOCATION = 'Redmond, WA 98052'
-RATING = 0.45
-DISTANCE = 2168.1
+  # Separator
+  separator = ttk.Separator(window, orient='horizontal')
+  separator.grid(row=1, columnspan=4, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='ew')
 
-# Left column
-frame_left = tk.Frame(master=window)
-frame_left.grid(row=2, column=0, sticky='nw')
+  # Left column
+  frame_left = tk.Frame(master=window)
+  frame_left.grid(row=2, column=0, sticky='nw')
 
-label_item_number = tk.Label(master=frame_left, text='Item #', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
-label_item_number.grid(row=0, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
-label_item_number_value = tk.Label(master=frame_left, text=f'{ITEM_NUMBER}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
-label_item_number_value.grid(row=0, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_item_number = tk.Label(master=frame_left, text='Item #', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
+  label_item_number.grid(row=0, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_item_number_value = tk.Label(master=frame_left, text=f'{itemNumber}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
+  label_item_number_value.grid(row=0, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
 
-label_title = tk.Label(master=frame_left, text='Title', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
-label_title.grid(row=1, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
-label_title_value = tk.Label(master=frame_left, text=f'{TITLE}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
-label_title_value.grid(row=1, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_title = tk.Label(master=frame_left, text='Title', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
+  label_title.grid(row=1, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_title_value = tk.Label(master=frame_left, text=f'{item_title}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
+  label_title_value.grid(row=1, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
 
-label_game_type = tk.Label(master=frame_left, text='Game Type', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
-label_game_type.grid(row=2, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
-label_game_type_value = tk.Label(master=frame_left, text=f'{GAME_TYPE}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
-label_game_type_value.grid(row=2, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_game_type = tk.Label(master=frame_left, text='Game Type', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
+  label_game_type.grid(row=2, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_game_type_value = tk.Label(master=frame_left, text=f'{itemtype_name}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
+  label_game_type_value.grid(row=2, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
 
-label_platform = tk.Label(master=frame_left, text='Platform', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
-label_platform.grid(row=3, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
-label_platform_value = tk.Label(master=frame_left, text=f'{PLATFORM}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
-label_platform_value.grid(row=3, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_platform = tk.Label(master=frame_left, text='Platform', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
+  label_platform.grid(row=3, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_platform_value = tk.Label(master=frame_left, text=f'{itemtype_platform}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
+  label_platform_value.grid(row=3, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
 
-label_media = tk.Label(master=frame_left, text='Media', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
-label_media.grid(row=4, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
-label_media_value = tk.Label(master=frame_left, text=f'{MEDIA}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
-label_media_value.grid(row=4, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_media = tk.Label(master=frame_left, text='Media', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
+  label_media.grid(row=4, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_media_value = tk.Label(master=frame_left, text=f'{itemtype_media}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
+  label_media_value.grid(row=4, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
 
-label_condition = tk.Label(master=frame_left, text='Condition', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
-label_condition.grid(row=5, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
-label_condition_value = tk.Label(master=frame_left, text=f'{CONDITION}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
-label_condition_value.grid(row=5, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_condition = tk.Label(master=frame_left, text='Condition', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
+  label_condition.grid(row=5, column=0, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
+  label_condition_value = tk.Label(master=frame_left, text=f'{item_condition}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
+  label_condition_value.grid(row=5, column=1, padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, sticky='w')
 
-# Right column
-frame_right = tk.Frame(master=window)
-frame_right.grid(row=2, column=1, sticky='nw')
+  # Right column
+  # frame_right = tk.Frame(master=window)
+  # frame_right.grid(row=2, column=1, sticky='nw')
 
-label_offered_by = tk.Label(master=frame_right, text='Offered by', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
-label_offered_by.grid(row=0, column=0, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
-label_offered_by_value = tk.Label(master=frame_right, text=f'{OFFERED_BY}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
-label_offered_by_value.grid(row=0, column=1, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
+  # label_offered_by = tk.Label(master=frame_right, text='Offered by', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
+  # label_offered_by.grid(row=0, column=0, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
+  # label_offered_by_value = tk.Label(master=frame_right, text=f'{OFFERED_BY}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
+  # label_offered_by_value.grid(row=0, column=1, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
 
-label_location = tk.Label(master=frame_right, text='Location', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
-label_location.grid(row=1, column=0, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
-label_location_value = tk.Label(master=frame_right, text=f'{LOCATION}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
-label_location_value.grid(row=1, column=1, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
+  # label_location = tk.Label(master=frame_right, text='Location', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
+  # label_location.grid(row=1, column=0, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
+  # label_location_value = tk.Label(master=frame_right, text=f'{LOCATION}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
+  # label_location_value.grid(row=1, column=1, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
 
-label_rating = tk.Label(master=frame_right, text='Rating', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
-label_rating.grid(row=2, column=0, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
-label_rating_value = tk.Label(master=frame_right, text=f'{RATING}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
-label_rating_value.grid(row=2, column=1, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
+  # label_rating = tk.Label(master=frame_right, text='Rating', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL))
+  # label_rating.grid(row=2, column=0, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
+  # label_rating_value = tk.Label(master=frame_right, text=f'{RATING}', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE))
+  # label_rating_value.grid(row=2, column=1, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
 
-label_distance = tk.Label(master=frame_right, text='Distance', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL), bg='red')
-label_distance.grid(row=3, column=0, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
-label_distance_value = tk.Label(master=frame_right, text=f'{DISTANCE} miles', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE), bg='red')
-label_distance_value.grid(row=3, column=1, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
+  # label_distance = tk.Label(master=frame_right, text='Distance', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_LABEL), bg='red')
+  # label_distance.grid(row=3, column=0, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
+  # label_distance_value = tk.Label(master=frame_right, text=f'{DISTANCE} miles', font=(LABEL_FONT_FAMILY, LABEL_FONT_SIZE, LABEL_FONT_WEIGHT_VALUE), bg='red')
+  # label_distance_value.grid(row=3, column=1, padx=WINDOW_PADDING_X+WINDOW_PADDING_X_OFFSET, pady=WINDOW_PADDING_Y, sticky='w')
 
-button_propose_swap = tk.Button(master=frame_right, text='Propose Swap', height=2, font=(BUTTON_FONT_FAMILY, BUTTON_FONT_SIZE, BUTTON_FONT_WEIGHT), fg='white', bg='blue')
-button_propose_swap.grid(row=4, columnspan=2, pady=25)
+  # button_propose_swap = tk.Button(master=frame_right, text='Propose Swap', height=2, font=(BUTTON_FONT_FAMILY, BUTTON_FONT_SIZE, BUTTON_FONT_WEIGHT), fg='white', bg='blue')
+  # button_propose_swap.grid(row=4, columnspan=2, pady=25)
 
 ##############################
 # EVENT LOOP
 ##############################
-if __name__ == "__main__":
-  window.mainloop()
+# if __name__ == "__main__":
+#   window.mainloop()
