76930a1d772f5b3a90d0370c5da4d2cd2819d978
dbui44 <dbui44@gatech.edu>
2022-04-16 09:30:23 -0400

    update swap_history

--- Phase_3/swap_history.py
+++ Phase_3/swap_history.py
@@ -1,5 +1,4 @@
 from global_variables import *
-from sql import sql_get_swap_history
 
 
 def view_item(userEmail):
@@ -14,8 +13,6 @@ def view_item(userEmail):
 
   TABLE_COLUMN_ANCHOR = tk.W
   TABLE_COLUMN_STRETCH = tk.NO
-
-  WINDOW_WIDTH = 800
 
 
   swap_summary_columns = ['My role', 'Total', 'Accepted', 'Rejected', 'Rejected %']
@@ -60,8 +57,6 @@ def view_item(userEmail):
 #   other_user = df['counterparty_email'].values[0]
 #   rating = df['item_condition'].values[0]
 
-
-
   ########## VIEW
 
   # Header
@@ -80,10 +75,31 @@ def view_item(userEmail):
   table_items.column('#0', width=0, stretch=TABLE_COLUMN_STRETCH)
   table_items.heading('#0', text='')
   for column in table_items['columns']:
-      table_items.column(column, anchor=TABLE_COLUMN_ANCHOR,idth=int(WINDOW_WIDTH/len(table_items['column']))-2)
+      table_items.column(column, anchor=TABLE_COLUMN_ANCHOR,idth=int(WINDOW_SIZE_WIDTH/len(table_items['column']))-2)
       table_items.heading(column, text=column, anchor=TABLE_COLUMN_ANCHOR)
   for i, row in enumerate(swap_summary_data):
       table_items.insert(parent='', index='end', iid=i, text='', values=row)
+  table_items.pack()
+
+
+  # Separator
+  separator = ttk.Separator(window, orient='horizontal')
+  separator.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, fill='x')
+
+  # Table
+  frame_items = tk.Frame(window)
+  frame_items.pack(padx=WINDOW_PADDING_X, pady=WINDOW_PADDING_Y, anchor='w')
+  table_items = ttk.Treeview(frame_items, height=len(swap_data))
+  table_items['columns'] = swap_history_columns
+  table_items.column('#0', width=0, stretch=TABLE_COLUMN_STRETCH)
+  table_items.heading('#0', text='')
+  for column in table_items['columns']:
+      table_items.column(column, anchor=TABLE_COLUMN_ANCHOR,
+                        width=int(WINDOW_SIZE_WIDTH/len(table_items['column']))-2)
+      table_items.heading(column, text=column, anchor=TABLE_COLUMN_ANCHOR)
+  for i, row in enumerate(swap_data):
+      table_items.insert(parent='', index='end', iid=i, text='', values=row)
+  table_items.pack()
  
 
 ##############################
