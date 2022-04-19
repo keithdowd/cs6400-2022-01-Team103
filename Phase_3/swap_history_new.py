2e57277ff2141ebee7a01a31804e76a29096978e
Nijasure, Pradeep <pnijasure3@gatech.edu>
2022-04-19 18:51:22 -0400

    corrected rejected %

--- Phase_3/swap_history_new.py
+++ Phase_3/swap_history_new.py
@@ -98,10 +98,10 @@ def swap_hist(userEmail):
     proposer['accepted_count'].reset_index(drop=True)[0]
     proposer_rejected_count = 0 if proposer['rejected_count'].empty else \
     proposer['rejected_count'].reset_index(drop=True)[0]
-
     proposer_total = proposer_accepted_count + proposer_rejected_count
     proposer_rejected_percent = 0 if proposer_accepted_count == 0 else round(
         (proposer_rejected_count / proposer_total) * 100)
+
 
 
 
@@ -114,8 +114,8 @@ def swap_hist(userEmail):
         (counter_rejected_count / counter_total) * 100)
 
 
-    # print("REJECT COUNTER HERE")
-    # print(counter_rejected_count)
+    print("REJECT COUNTER HERE")
+    print(counter_rejected_count)
     item_counts['My role'] = 'Proposer'
     item_counts['Total'] = proposer_total
     item_counts['Accepted'] = proposer_accepted_count
@@ -176,11 +176,10 @@ def swap_hist(userEmail):
 
     # Table (Values)#Propose
     for col_index, item_count in enumerate(item_counts.values()):
-        print('PRINTING INDEX')
-        print(col_index)
         table_item_counts_value = tk.Label(
             master=window,
-            text=item_count, bg=('red' if col_index==4 and item_counts['Rejected %'] >= 50.0 else None),
+            text=item_count,
+            fg="red" if col_index==4 and item_count>=50 else "black",
             font=(
                 LABEL_FONT_FAMILY,
                 LABEL_FONT_SIZE,
@@ -195,10 +194,11 @@ def swap_hist(userEmail):
 
     # Table (Values)#counter
     for col_index, item_count in enumerate(counter_item_counts.values()):
-        # print(counter_item_counts.values())
+        print(counter_item_counts.values())
         counter_table_item_counts_value = tk.Label(
             master=window,
-            text=item_count, bg=('red' if col_index==4 and counter_item_counts['Rejected %'] >= 50.0 else None),
+            text=item_count,
+            fg="red" if col_index == 4 and item_count >= 50 else "black",
             font=(
                 LABEL_FONT_FAMILY,
                 LABEL_FONT_SIZE,
@@ -210,6 +210,8 @@ def swap_hist(userEmail):
             padx=5,
             pady=10,
             sticky='ew')
+
+
 
     ##############################
     # MY ITEMS
@@ -233,26 +235,26 @@ def swap_hist(userEmail):
 
     def callback(i):
         rating = clicked[i].get()
-        # print("rating changed to:", rating)
+        print("rating changed to:", rating)
         return rating
 
     def dummy( swapID,userEmail):
         print(swapID)
 
     def buttonclick(rating,swapid):
-        # print("inside")
-        # print(rating)
-        # print(swapid)
+        print("inside")
+        print(rating)
+        print(swapid)
         window.destroy()
         mycursor = cnx.cursor()
         query = sql_rate_my_unrated_swaps_counterparty(userEmail, swapid, rating)
-        # print(query)
+        print(query)
         mycursor.execute(query)
         cnx.commit()
         mycursor.close()
         mycursor = cnx.cursor()
         query = sql_rate_my_unrated_swaps_proposer(userEmail, swapid, rating)
-        # print(query)
+        print(query)
         mycursor.execute(query)
         cnx.commit()
         mycursor.close()
@@ -266,15 +268,15 @@ def swap_hist(userEmail):
     my_items_data = []
     my_items_data1=[]
     my_items_data_rating = []
-    # print(df['swap_counterparty_rating'],df['swap_proposer_rating'],df['swapID'])
+    print(df['swap_counterparty_rating'],df['swap_proposer_rating'],df['swapID'])
     for index, row1 in df.iterrows():
         flag=0
         flag1=0
         #rowcnt=0
-        if (pd.isna(row1['swap_counterparty_rating']) == True and row1['swap_date_responded'] != '' and row1[
-            'swap_status'] == 'Accepted' and row1['counterparty_email']==userEmail) or (
-                pd.isna(row1['swap_proposer_rating']) == True and row1['swap_date_proposed'] != '' and row1[
-            'swap_status'] == 'Accepted' and row1['proposer_email']==userEmail):
+        if (pd.isna(row1['swap_counterparty_rating']) == True and row1['swap_date_proposed'] != '' and row1[
+            'swap_status'] == 'Accepted' and row1['proposer_email']==userEmail) or (
+                pd.isna(row1['swap_proposer_rating']) == True and row1['swap_date_responded'] != '' and row1[
+            'swap_status'] == 'Accepted' and row1['counterparty_email']==userEmail):
 
             flag=1
         if pd.isna(row1['swap_proposer_rating']) == True:
@@ -282,8 +284,8 @@ def swap_hist(userEmail):
         if flag==0:
 
 
-            # print(row1['swapID'], row1['swap_counterparty_rating'], pd.isna(row1['swap_counterparty_rating']),flag)
-            # print(row1['swapID'], row1['swap_proposer_rating'], pd.isna(row1['swap_proposer_rating']),flag1)
+            print(row1['swapID'], row1['swap_counterparty_rating'], pd.isna(row1['swap_counterparty_rating']),flag)
+            print(row1['swapID'], row1['swap_proposer_rating'], pd.isna(row1['swap_proposer_rating']),flag1)
             swapid = row1['swapID']
             proposed_date = row1['swap_date_proposed']
             accepted_rejected_date = row1['swap_date_responded']
@@ -340,7 +342,7 @@ def swap_hist(userEmail):
     # Header
     label_my_items = tk.Label(
         master=window,
-        text='My Items',
+        text='My Swaps',
         font=(
             LABEL_FONT_FAMILY,
             LABEL_FONT_SIZE,
@@ -390,7 +392,7 @@ def swap_hist(userEmail):
 
 
             for col_index, my_item_value in enumerate(my_item):
-                # print(my_item_value)
+                print(my_item_value)
                 # center item number and left justify all other fields
                 if col_index == 0:
                     anchor = 'center'
@@ -436,9 +438,9 @@ def swap_hist(userEmail):
             )
             rowcnt2+=1
     for index, row in df.iterrows():
-    #    print(row)
-       if (pd.isna(row['swap_counterparty_rating']) == True and row['swap_date_responded'] !='' and row['swap_status']=='Accepted'  and row['counterparty_email']==userEmail) or(pd.isna(row['swap_proposer_rating']) == True and  row['swap_date_proposed'] !=''  and row['swap_status']=='Accepted'  and row['proposer_email']==userEmail):
-        #  print(row['swapID'], row['swap_counterparty_rating'], pd.isna(row['swap_counterparty_rating']))
+       print(row)
+       if (pd.isna(row['swap_counterparty_rating']) == True and row['swap_date_proposed'] !='' and row['swap_status']=='Accepted'  and row['proposer_email']==userEmail) or(pd.isna(row['swap_proposer_rating']) == True and  row['swap_date_responded'] !=''  and row['swap_status']=='Accepted'  and row['counterparty_email']==userEmail):
+         print(row['swapID'], row['swap_counterparty_rating'], pd.isna(row['swap_counterparty_rating']))
          #print(row['swapid'], row['swap_proposer_rating'], pd.isna(row['swap_proposer_rating']))
 
           # print("start here")
@@ -500,7 +502,7 @@ def swap_hist(userEmail):
     # Header
     label_my_items = tk.Label(
         master=window,
-        text='My Items',
+        text='My Swaps',
         font=(
             LABEL_FONT_FAMILY,
             LABEL_FONT_SIZE,
@@ -551,7 +553,7 @@ def swap_hist(userEmail):
             r = 12 + rowcnt1
             row_index += r
              # layout starts at row 8
-            # print(my_item)
+            print(my_item)
 
 
             for col_index, my_item_value in enumerate(my_item):
@@ -647,7 +649,7 @@ def swap_hist(userEmail):
 ##############################
 # EVENT LOOP
 ##############################
-# swap_hist('usr001@gt.edu')
+#swap_hist('usr001@gt.edu')
 
 
 '''    else:  # Show a message instead of the items table if the user has no items
